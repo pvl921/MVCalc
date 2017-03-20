32db580baaaab53d85f3ec715ec5f2bcda0ebf78
pvl921 <pvl921@gmail.com>
2017-03-21 01:10:26 +0300

    Add files via upload

--- src/Model/Data.cs
+++ src/Model/Data.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 
-
 namespace MVCalc
 {
     public class Data
@@ -8,8 +7,7 @@ namespace MVCalc
         public double Operand1;
         public double Operand2;
         public double Result;
-        public readonly char[] AvailOps = {'+', '-', '*', '/', '^'};
-
+        
         public bool ParseOperand(string s, int i)
         {
             if (i == 1) return double.TryParse(s, out Operand1); 
@@ -17,11 +15,10 @@ namespace MVCalc
             return false;
         }
 
-        public int ParseOperator(string s)
+        public char? ParseOperator(string s)
         {
-            if (s.Length!=1) return -1;
-            char _temp = s[0];
-            return Array.FindIndex(AvailOps, n => n == _temp);
+            if (s.Length!=1) return null;
+            return s[0];
         }
 
     }
