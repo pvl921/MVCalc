152ef6d5dc5b6b2262abfa9bdd617faa0454cf9c
pvl921 <pvl921@gmail.com>
2017-03-27 01:27:00 +0300

    Add files via upload

---
+++ src/MVCalc/Controller/DisplayController.cs
@@ -0,0 +1,71 @@
+﻿namespace MVCalc
+{
+    class DisplayController
+    {
+        enum InputTypes { Operand, Operator };
+
+        ///<summary>
+        ///Формирует текстовые сообщения для ввода данных и пояснения для пользователя.
+        ///</summary>
+        public static void GetInput(bool firstTime, int inputType, string operandNumber = "первый")
+        {
+            if (firstTime)
+            {
+                View.Render(0, "Welcome to MVC Calculator.\nEnter first operand(x), then a math operator to be applied, and then the second operand(y).\nType \"Exit\" to quit the program.\n" +
+                    "Available operators:\nx + y\nx - y\nx * y\nx / y\nx ^ y\n\n");
+            };
+
+            InputTypes _inputType = (InputTypes)inputType;
+            switch (_inputType)
+            {
+                case InputTypes.Operand:
+                    View.Render(1, ($"Введите {operandNumber} операнд:\t\t"));
+                    break;
+                case InputTypes.Operator:
+                    View.Render(2, ($"Введите оператор:\t\t"));
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        ///<summary>
+        ///Формирует текстовые сообщения об ошибках в типе данных.
+        /// </summary>
+        public static void ParseException(string message)
+        {
+            View.Render(4, $"ОШИБКА!\t{message}\n\n");
+        }
+
+        ///<summary>
+        ///Передает данные в модель.
+        /// </summary>
+        public static void UpdateModel(Data data, double result, bool resultOK)
+        {
+            data.Result = result;
+            data.ResultOK = resultOK;
+        }
+
+        ///<summary>
+        ///Формирует текстовые сообщения для вывода результата.
+        /// </summary>
+        public static void ProcessResult(Data data)
+        {
+            if (!data.ResultOK)
+            {
+                View.Render(4, "ОШИБКА! Недопустимый символ оператора или несовместимое значение операнда. Выражение не вычислено.\n\n");
+                return;
+            }
+            View.Render(3, $"Результат:\t\t\t{data.Result}\n\n");
+        }
+
+        ///<summary>
+        ///Формирует текстовое сообщение о завершении программы.
+        /// </summary>
+        public static void Quit()
+        {
+            View.Render(0, "...quitting...\n");
+        }
+
+    }
+}
